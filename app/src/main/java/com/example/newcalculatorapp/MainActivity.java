233b9b7efdf5584a1ebe7876a6dabad8d1f618fc
meAhedonist8616 <sreyasgiri24704@gmail.com>
2024-12-18 12:42:36 +0530

    updation

--- app/src/main/java/com/example/newcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/newcalculatorapp/MainActivity.java
@@ -14,17 +14,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1;
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         b1=(Button) findViewById(R.id.addbut);
+        b2=(Button)findViewById(R.id.subbut);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(), SubActivity.class);
                 startActivity(i);
             }
         });
